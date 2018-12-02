39a436103fbabb78dddd595cbc52ac7e1a8bfb2a
Gordon MacMillan <gmacilla@ymail.com>
2018-12-02 15:10:21 -0700

    docker entrypoint change to rebuild image from scratch

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,9 +2,10 @@
 
 cd /workspace/job
 
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+git config --global user.email "user@email.com"
+git config --global user.name "name"
 git remote set-url origin https://github.com/DMLSG/human-protein-atlas.git
 git pull
+git checkout $BRANCH
 
 ./train_script.sh
