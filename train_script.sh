cd9f6d955fef814f2da6951a36f966f55a0db1b7
Gordon MacMillan <gmacilla@ymail.com>
2018-12-09 08:23:56 -0700

    test to see if test.py works

--- train_script.sh
+++ train_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "*** running train script ***"
-
-python -u train.py -n "vgg16" -d "hpa" -p "True" --train-images-path="/hpakf-image-data/data/train_images" --nEpochs=1 --batchSz=32 --use-cuda=yes
-
-echo "*** training complete. pod will stop in 10 minutes ***"
-# echo "*** training complete. running test script ***"
-
-# python -u test.py -n "vgg16" -d "hpa" -p "True" --test-images-path="/hpakf-image-data/data/test_images"
+# echo "*** running train script ***"
 #
-# echo "*** testing complete. pod will stop in 10 minutes ***"
+# python -u train.py -n "vgg16" -d "hpa" -p "True" --train-images-path="/hpakf-image-data/data/train_images" --nEpochs=1 --batchSz=32 --use-cuda=yes
+#
+# echo "*** training complete. pod will stop in 10 minutes ***"
+echo "*** training complete. running test script ***"
+
+python -u test.py -n "vgg16" -d "hpa" -p "True" --test-images-path="/hpakf-image-data/data/test_images"
+
+echo "*** testing complete. pod will stop in 10 minutes ***"
 
 sleep 600
