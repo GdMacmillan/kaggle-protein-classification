a6c92f9048d3ff188204d857852026b335434d60
Gordon MacMillan <gmacilla@ymail.com>
2018-12-30 11:29:58 -0700

    added distributed train script

---
+++ train_script_distributed.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+./setup_cloud_storage.sh
+
+echo "*** running train script ***"
+
+python -u train.py -n "vgg16" -d "hpa" -p "True" --train-images-path="/hpakf-image-data/data/train_images" --nEpochs=1 --batchSz=32 --use-cuda=yes --crit="crl" --distributed "True"
+
+echo "*** training complete. running test script ***"
+
+GOOGLE_APPLICATION_CREDENTIALS=/workspace/gcs_credentials.json python -u test.py -n "vgg16" -d "hpa" -p "True" --batchSz=32 --test-images-path="/hpakf-image-data/data/test_images"
+
+echo "*** testing complete. pod will stop in 10 minutes ***"
+
+sleep 600
